6e21a539df97577ef8b8da0f62a30b99fa1bc64f
Nathan Baird <nate19981@googlemail.com>
2022-09-06 23:25:22 +0100

    Custom sized grids now functional in a basic way

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 const container = document.querySelector('#container');
 
-//pixel contains an array-like object with each div/pixel in the grid
-const pixel = document.getElementsByClassName('gridSquare');
-
 //Values for creating the grid
-const GRID_LENGTH = 16;
-const GRID_VOLUME = GRID_LENGTH * GRID_LENGTH;
+let GRID_LENGTH = 16;
+let GRID_VOLUME = GRID_LENGTH * GRID_LENGTH;
 
 //Updates container div style to divide the grid appropriately given the GRID_LENGTH
 container.style.gridTemplateColumns = `repeat(${GRID_LENGTH}, 0fr)`;
 
-createCanvisGrid();
+//createCanvisGrid();
 
+updatePixelCount();
 //Checks if the users mouse is hovering over any pixel in the grid
 for(let i = 0; i < pixel.length; i++) {
     pixel[i].addEventListener('mouseover', () => {
@@ -23,7 +21,7 @@ for(let i = 0; i < pixel.length; i++) {
 function createCanvisGrid() {
     //Creates each individual 'pixel' in the grid.
     for(let i = 0; i < GRID_VOLUME; i++) {
-        const gridSquare = document.createElement('div');
+        let gridSquare = document.createElement('div');
 
         gridSquare.classList.add('gridSquare');
         gridSquare.style.height = '2rem';
@@ -33,4 +31,24 @@ function createCanvisGrid() {
 
         container.appendChild(gridSquare);
     }
+}
+
+function selectGridSize() {
+    let newGridSize = prompt('Please enter the length of your canvas in pixels. (Max size: 100)');
+    //converts input to an integer number
+    newGridSize = Number(newGridSize);
+    
+    if(!(newGridSize < 0) && !(newGridSize > 100)) {
+        GRID_LENGTH = newGridSize;
+        GRID_VOLUME = GRID_LENGTH * GRID_LENGTH;
+        createCanvisGrid();
+        container.style.gridTemplateColumns = `repeat(${GRID_LENGTH}, 0fr)`;
+        pixel = updatePixelCount();
+    }
+}
+
+function updatePixelCount() {
+    //pixel contains an array-like object with each div/pixel in the grid
+    pixel = null;
+    pixel = document.getElementsByClassName('gridSquare');   
 }
