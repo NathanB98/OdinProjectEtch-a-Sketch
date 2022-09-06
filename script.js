d078296e16e341d3ddf6abe603381c0cd30e4e3c
Nathan Baird <nate19981@googlemail.com>
2022-09-06 22:49:02 +0100

    Pixels now change colour on mouse over

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const container = document.querySelector('#container');
 
+//pixel contains an array-like object with each div/pixel in the grid
+const pixel = document.getElementsByClassName('gridSquare');
+
 //Values for creating the grid
 const GRID_LENGTH = 16;
 const GRID_VOLUME = GRID_LENGTH * GRID_LENGTH;
@@ -9,13 +12,20 @@ container.style.gridTemplateColumns = `repeat(${GRID_LENGTH}, 0fr)`;
 
 createCanvisGrid();
 
+//Checks if the users mouse is hovering over any pixel in the grid
+for(let i = 0; i < pixel.length; i++) {
+    pixel[i].addEventListener('mouseover', () => {
+        pixel[i].style.backgroundColor = 'Red';
+    }
+)}
+
 //Creates a 1:1 grid of pixels given a value from GRID_LENGTH
 function createCanvisGrid() {
     //Creates each individual 'pixel' in the grid.
     for(let i = 0; i < GRID_VOLUME; i++) {
         const gridSquare = document.createElement('div');
 
-        gridSquare.classList.add('gridSqure');
+        gridSquare.classList.add('gridSquare');
         gridSquare.style.height = '2rem';
         gridSquare.style.width = '2rem';
         gridSquare.style.backgroundColor = 'Blue';
