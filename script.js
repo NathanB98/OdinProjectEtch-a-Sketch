d27ffaa21a8183a12ab3cf92affa123235a7f1ca
Nathan Baird <nate19981@googlemail.com>
2022-09-06 22:24:47 +0100

    Refactor of script. 16x16 grid now showing as expected. CSS for grid added

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 const container = document.querySelector('#container');
 
-for(let i = 0; i < 16; i++) {
-    makeSquare();
-}
+//Values for creating the grid
+const GRID_LENGTH = 16;
+const GRID_VOLUME = GRID_LENGTH * GRID_LENGTH;
 
-function makeSquare() {
-    const gridSquare = document.createElement('div');
+//Updates container div style to divide the grid appropriately given the GRID_LENGTH
+container.style.gridTemplateColumns = `repeat(${GRID_LENGTH}, 0fr)`;
 
-    gridSquare.classList.add('gridSqure');
-    gridSquare.style.height = '2rem';
-    gridSquare.style.width = '2rem';
-    gridSquare.style.backgroundColor = 'Blue';
-    gridSquare.style.border = 'Solid Black'
+createCanvisGrid();
 
-    container.appendChild(gridSquare);
+//Creates a 1:1 grid of pixels given a value from GRID_LENGTH
+function createCanvisGrid() {
+    //Creates each individual 'pixel' in the grid.
+    for(let i = 0; i < GRID_VOLUME; i++) {
+        const gridSquare = document.createElement('div');
+
+        gridSquare.classList.add('gridSqure');
+        gridSquare.style.height = '2rem';
+        gridSquare.style.width = '2rem';
+        gridSquare.style.backgroundColor = 'Blue';
+        gridSquare.style.border = 'Solid Black'
+
+        container.appendChild(gridSquare);
+    }
 }
